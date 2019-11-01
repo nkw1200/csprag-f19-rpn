294b2d3a6baf3b3928ee3cada5e937107eaa690c
Nathan Wong <wongna@umich.edu>
2019-11-01 14:24:20 -0400

    Implement division tests
    Because this is an integer only calculatorm division has multiple behaviors.
    When things come out even, no truncation is needed, but when a floating point value would result, floor occurs.

--- test_rpn.py
+++ test_rpn.py
@@ -8,6 +8,15 @@ class TestBasics(unittest.TestCase):
     def test_sub(self):
         result = rpn.calculate("5 3 -")
         self.assertEqual(2, result)
+    def test_mul(self):
+        result = rpn.calculate("12 8 *")
+        self.assertEqual(96, result)
+    def test_div1(self):
+        result = rpn.calculate("12 8 /")
+        self.assertEqual(1, result)
+    def test_div2(self):
+        result = rpn,calculate("24 8 /")
+        self.assertEqual(3, result)
     def test_badinput(self):
         with self.assertRaises(TypeError):
             rpn.calculate('1 2 3 +')
