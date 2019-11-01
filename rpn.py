57c6a4213f3339fed86c97038591ab75babbcc6b
Nathan Wong <wongna@umich.edu>
2019-11-01 14:05:56 -0400

    Eliminate handwritten functions for library functions

--- rpn.py
+++ rpn.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
+import operator
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+}
+
 def calculate(arg):
     stack = list()
     for token in arg.split():
-        print(token)
-        if token == '+':
+        try:
+            value = int(token)
+            stack.append(value)
+        except ValueError:
+            function = operators[token]
             arg2 = stack.pop()
             arg1 = stack.pop()
-            result = arg1 + arg2
+            result = function(arg1, arg2)
             stack.append(result)
-        elif token == '-':
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = arg1 - arg2
-            stack.append(result)
-        else:
-            stack.append(int(token))
 
        # print(stack)
     if len(stack) != 1:
